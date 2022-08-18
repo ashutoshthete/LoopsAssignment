8113447095db2d72db2c00de3b791325e8d3bd37
ashutoshthete <ashutoshthete24@gmail.com>
2022-08-18 11:13:23 +0530

    saving to github 18/8/22

--- LoopsAssignment/GcdandLcm.cs
+++ LoopsAssignment/GcdandLcm.cs
@@ -6,15 +6,39 @@ namespace LoopsAssignment
 {
     class GcdandLcm
     {
+      
         public static void Main(string[] args)
         {
-            int a; 
-            int b;
-            int c;
+            int num1, num2, gcd, lcm, remainder, numerator, denominator;
             Console.WriteLine("Enter first numbers ");
-             a= int.Parse(Console.ReadLine());
+            num1= int.Parse(Console.ReadLine());
             Console.WriteLine("Enter second numbers ");
-            b = int.Parse(Console.ReadLine());
+            num2 = int.Parse(Console.ReadLine());
+
+            if (num1 > num2)
+            {
+                numerator = num1;
+                denominator = num2;
+            }
+            else
+            {
+                numerator = num2;
+                denominator = num1;
+            }
+
+            remainder = numerator % denominator;
+
+            while (remainder != 0)
+            {
+                numerator = denominator;
+                denominator = remainder;
+                remainder = numerator % denominator;
+
+            }
+            gcd = denominator;
+            lcm = num1 * num2 / gcd;
+            Console.WriteLine("GCD of "+num1+" and "+num2+" is :"+gcd);
+            Console.WriteLine("LCM of "+num1+" and "+num2+" is :"+lcm);
 
             
 
---
+++ LoopsAssignment/Palindrome100to500.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LoopsAssignment
+{
+    class Palindrome100to500
+    {
+        public static void Main(string[] args)
+        {
+            int range1 = 100;
+            int range2 = 500;
+            int num1 = 0;
+            int num2 = 0;
+            for(int i = range1; i <= range2; i++)
+            {
+                num1 = i;
+                num2 = 0;
+                while (num1 != 0)
+                {
+                    int rem = num1 % 10;
+                    num1 /= 10;
+                    num2 = num2 * 10 + rem;
+                }
+                if (i == num2)
+                {
+                    Console.WriteLine(i+" ");
+                }
+            }
+            
+        }
+        
+
+    }
+}
+
